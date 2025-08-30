bc51436fd045dad01ab9d1db4f6068fe7b00315d
ranggaaditia2807 <ranggaaditiya282007@gmail.com>
2025-08-30 05:33:19 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/gandul-gudang-sentosa/" : "/",
+  base: mode === "production" ? "/gudang-pln-gandul/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
