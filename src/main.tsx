886b26a5d074dea5c02c8266028b44237baaa966
ranggaaditia2807 <ranggaaditiya282007@gmail.com>
2025-09-03 10:53:02 -0700

    Update BrowserRouter basename for local and production environments

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import App from "./App";
 import "./index.css"; // pastikan Tailwind / CSS sudah import
 import { UserProvider } from "./contexts/UserContext";
 
+const basename = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+  ? "/"
+  : "/gudang-pln-gandul/";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/gudang-pln-gandul/">
+    <BrowserRouter basename={basename}>
       <UserProvider>
         <App />
       </UserProvider>
